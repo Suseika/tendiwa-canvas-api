74a272fb65fb25974dd1fe74858d79e910d907f9
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-03 07:05:54 +0500

    Implement drawing a SegmentGroup

--- src/main/kotlin/org/tendiwa/canvas/algorithms/geometry/DrawingGeometry.kt
+++ src/main/kotlin/org/tendiwa/canvas/algorithms/geometry/DrawingGeometry.kt
@@ -2,10 +2,15 @@ package org.tendiwa.canvas.algorithms.geometry
 
 import org.tendiwa.canvas.api.Canvas
 import org.tendiwa.plane.geometry.polygons.Polygon
+import org.tendiwa.plane.geometry.shapes.SegmentGroup
 import java.awt.Color
 
 fun Canvas.draw(polygon: Polygon, color: Color) {
     polygon.segments.forEach {
         this.draw(it, color)
     }
+}
+
+fun Canvas.draw(segmentGroup: SegmentGroup, color: Color) {
+    segmentGroup.segments.forEach { draw(it, color) }
 }

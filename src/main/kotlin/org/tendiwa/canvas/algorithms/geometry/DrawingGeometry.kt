0b64dcc6df2f48f7afa1c457284c94e5418f1468
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-03 07:42:22 +0500

    Implement drawing points with particular radius

--- src/main/kotlin/org/tendiwa/canvas/algorithms/geometry/DrawingGeometry.kt
+++ src/main/kotlin/org/tendiwa/canvas/algorithms/geometry/DrawingGeometry.kt
@@ -1,6 +1,8 @@
 package org.tendiwa.canvas.algorithms.geometry
 
 import org.tendiwa.canvas.api.Canvas
+import org.tendiwa.plane.geometry.circles.Circle
+import org.tendiwa.plane.geometry.points.Point
 import org.tendiwa.plane.geometry.polygons.Polygon
 import org.tendiwa.plane.geometry.shapes.SegmentGroup
 import java.awt.Color
@@ -13,4 +15,8 @@ fun Canvas.draw(polygon: Polygon, color: Color) {
 
 fun Canvas.draw(segmentGroup: SegmentGroup, color: Color) {
     segmentGroup.segments.forEach { draw(it, color) }
+}
+
+fun Canvas.draw(point: Point, color: Color, radius: Double) {
+    draw(Circle(point, radius), color)
 }

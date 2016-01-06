29b801ce55f14286ce2435db03c31acf69e145e1
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-07 03:30:17 +0500

    Rename Ray.firstIntersection(Rectangle) to Ray.closestIntersection(Rectangle)

--- src/main/kotlin/org/tendiwa/canvas/algorithms/geometry/DrawingRays.kt
+++ src/main/kotlin/org/tendiwa/canvas/algorithms/geometry/DrawingRays.kt
@@ -4,7 +4,7 @@ import org.tendiwa.canvas.api.Canvas
 import org.tendiwa.derasterization.toRectangle
 import org.tendiwa.plane.geometry.points.distanceTo
 import org.tendiwa.plane.geometry.rays.Ray
-import org.tendiwa.plane.geometry.rays.firstIntersection
+import org.tendiwa.plane.geometry.rays.closestIntersection
 import org.tendiwa.plane.geometry.rays.segment
 import org.tendiwa.plane.geometry.rectangles.contains
 import org.tendiwa.plane.grid.constructors.GridRectangle
@@ -19,7 +19,7 @@ fun Canvas.drawRay(ray: Ray, color: Color) {
                 draw(ray.start, color, 2.0)
             }
         }
-        .let { ray.firstIntersection(it) }
+        .let { ray.closestIntersection(it) }
         .apply {
             if (this != null) {
                 draw(ray.segment(ray.start.distanceTo(this) + 1.0), color)
